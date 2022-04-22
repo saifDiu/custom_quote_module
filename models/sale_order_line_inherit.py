b3048cd85841dc3d5d95bf1fbb59834ecbc2b00f
saifDiu <alsaif839@gmail.com>
2022-04-22 22:48:44 +0600

    update

--- models/sale_order_line_inherit.py
+++ models/sale_order_line_inherit.py
@@ -4,10 +4,13 @@ from odoo import models,api,fields,_
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order.line'
 
-    height_prod = fields.Float(string="Height")
-    width_prod = fields.Float(string="Width")
+    height_prod = fields.Float(string="Alto")
+    width_prod = fields.Float(string="Ancho")
+    mando = fields.Text(string="mando")
+    motor = fields.Boolean(string="motor")
+    encajonda = fields.Boolean(string="Encajonada")
 
-    product_uom_qty = fields.Float(string='M2', digits='Product Unit of Measure', required=True, default=0.0)
+    product_uom_qty = fields.Float(string='m2', digits='Product Unit of Measure', required=True, default=0.0)
 
     @api.onchange('height_prod','width_prod')
     def onchange_height_width(self):
